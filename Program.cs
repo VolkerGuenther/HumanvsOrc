c20a41d1fcd7c412f3bdd4c7b7ab128fc32d20fe
Volker Günther <volker_guenther@web.de>
2020-04-18 22:34:11 +0200

    Update Fight

--- Program.cs
+++ Program.cs
@@ -1,143 +1,67 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
+
 
 namespace HumanvsOrc
 {
-    class Program
+    class Version2_Program
     {
         static void Main()
         {
             List<Underlyings> Heroes = new List<Underlyings>();
             {
-                // --> ( Attackvalue, Paradevalue , Lifepoints, Damage)
-                Heroes.Add(new Underlyings(15, 14, 25, 6));
-                Heroes.Add(new Underlyings(14, 15, 24, 5));
-                Heroes.Add(new Underlyings(16, 12, 33, 7));
+                // --> ( Attackvalue, Paradevalue , Lifepoints, Damage, Name)
+                Heroes.Add(new Underlyings(15, 7, 5, 6, "Hans"));
+                Heroes.Add(new Underlyings(14, 5, 4, 5, "Bertram"));
+                Heroes.Add(new Underlyings(16, 8, 33, 7, "Caesar"));
 
             }
             List<Underlyings> Orcs = new List<Underlyings>();
             {
-                // --> ( Attackvalue, Paradevalue , Lifepoints, Damage)
-                Orcs.Add(new Underlyings(8, 7, 16, 4));
-                Orcs.Add(new Underlyings(12, 6, 20, 3));
-                Orcs.Add(new Underlyings(8, 7, 15, 3));
-                Orcs.Add(new Underlyings(11, 4, 23, 4));
-                Orcs.Add(new Underlyings(15, 14, 15, 6));
+                // --> ( Attackvalue, Paradevalue , Lifepoints, Damage, Name)
+                Orcs.Add(new Underlyings(8, 7, 16, 4, "Fully"));
+                Orcs.Add(new Underlyings(12, 6, 5, 3, "Gully"));
+                Orcs.Add(new Underlyings(8, 7, 5, 3, "Strully"));
+                Orcs.Add(new Underlyings(11, 4, 3, 4, "Pully"));
+                Orcs.Add(new Underlyings(15, 14, 5, 6, "Rully"));
+
             }
 
-            Console.WriteLine($"Startwerte: ");
-            Console.WriteLine($"Daten vom Ork : {Orcs[0].getdateRed()}");
-            Console.WriteLine($"Daten vom Ork 1 : {Orcs[1].getdateRed()}");
-            Console.WriteLine($"Daten vom Hero : {Heroes[0].getdateRed()}");
-            Console.WriteLine($"Daten vom Hero 1 : {Heroes[1].getdateRed()} \n \n");
-
-            Fight_Round_Damage(Heroes[1], Heroes[2]);
-
-
-            while (Heroes[0].Lifepoints >= 0) // Falls Held stirbt, wird die Schleife abgebrochen
+            foreach (var Hero in Heroes)
             {
-                Random A_D_rnd = new Random();
-                int AttackrndH = A_D_rnd.Next(1, 20); // Attacke Held - Random
-                int DefendrndH = A_D_rnd.Next(1, 20); // Parade Held - Random
-                int AttackrndO = A_D_rnd.Next(1, 20); // Attacke Orc - Random
-                int DefendrndO = A_D_rnd.Next(1, 20); // Parade Orc - Random
 
-                //int AttackrndH = 12;
-                //int DefendrndH = 12;
-                //int AttackrndO = 10;
-                //int DefendrndO = 10;
 
-                //---------------Attacke Held----------------
-                if (Orcs[0].Lifepoints <= 0)
+                foreach (var Orc in Orcs)
                 {
-                    if (Orcs[1].Lifepoints <= 0) // Falls Orc stribt -> Schleife wird abgebrochen
+                    if (!Hero.Is_Alive())
                     {
                         break;
                     }
-                    //Hero Attacke gelungen - Orc1 Parade fehlgeschlagen
-                    else if (AttackrndH <= Heroes[0].Attackvalue && DefendrndO > Orcs[1].Paradevalue)
+                    while (Orc.Lifepoints > 0)
                     {
-                        Orcs[1].Lifepoints = Orcs[1].Lifepoints - Heroes[0].Damage;
-                        Console.WriteLine($"Aktion Held -> Attacke gelungen: Ork1 hat {Orcs[1].Lifepoints} Lebenspunkte ");
+                        Fight_Round_Damage(Hero, Orc);
+                        Console.WriteLine($"{Hero.Name}     VS  {Orc.Name}");
+                        Console.WriteLine($"Attack-> Orc: {Orc.Lifepoints}");
+                        if (!Orc.Is_Alive())
+                        {
+                            break;
+                        }
+
+                        Fight_Round_Damage(Orc, Hero);
+                        Console.WriteLine($"{Hero.Name}     VS  {Orc.Name}");
+                        Console.WriteLine($"Attack -> Hero: {Hero.Lifepoints}");
+                        if (!Hero.Is_Alive())
+                        {
+                            break;
+                        }
+
+                        Console.WriteLine("_________________________________________________________________________________________");
                     }
-                    //Hero Attacke parrierd
-                    else if (AttackrndH <= Heroes[0].Attackvalue && DefendrndO <= Orcs[1].Paradevalue)
-                    {
-                        Console.WriteLine("Aktion Held -> Angriff vom Held parriert");
-                    }
-                    // Hero Attacke misslungen
-                    else if (AttackrndH > Heroes[0].Attackvalue)
-                    {
-                        Console.WriteLine("Aktion Held -> Attacke auf dem Ork1 misslungen");
-                    }
-                }
-                //Hero Attacke gelungen - Orc Parade fehlgeschlagen
-                else if (AttackrndH <= Heroes[0].Attackvalue && DefendrndO > Orcs[0].Paradevalue)
-                {
-                    Orcs[0].Lifepoints = Orcs[0].Lifepoints - Heroes[0].Damage;
-                    Console.WriteLine($"Aktion Held -> Attacke gelungen: Ork hat {Orcs[0].Lifepoints} Lebenspunkte ");
-                }
-                //Hero Attacke parrierd
-                else if (AttackrndH <= Heroes[0].Attackvalue && DefendrndO <= Orcs[0].Paradevalue)
-                {
-                    Console.WriteLine("Aktion Held -> Angriff vom Held parriert");
-                }
-                // Hero Attacke misslungen
-                else if (AttackrndH > Heroes[0].Attackvalue)
-                {
-                    Console.WriteLine("Aktion Held -> Attacke auf den Ork misslungen");
+
                 }
 
-
-                //-------------------Attacke Orc----------
-                if (Orcs[0].Lifepoints <= 0)
-                {
-                    if (Orcs[1].Lifepoints <= 0) // Falls Orc stribt -> Schleife wird abgebrochen
-                    {
-                        break;
-                    }
-                    //Orc1 Attacke gelungen - Held Parade fehlgeschlagen
-                    else if (AttackrndO <= Orcs[1].Attackvalue && DefendrndH > Heroes[0].Paradevalue)
-                    {
-                        Heroes[0].Lifepoints = Heroes[0].Lifepoints - Orcs[1].Damage;
-                        Console.WriteLine($"Aktion Ork1 -> Attacke gelungen: Hero hat {Heroes[0].Lifepoints} Lebenspunkte " + "\n");
-                    }
-                    //Orc1 Attacke parrierd
-                    else if (AttackrndO <= Orcs[1].Attackvalue && DefendrndH <= Heroes[0].Paradevalue)
-                    {
-                        Console.WriteLine("Aktion Ork1 -> Angriff vom Ork 1 parriert" + "\n");
-                    }
-                    // Orc1 Attacke misslungen
-                    else if (AttackrndO > Orcs[1].Attackvalue)
-                    {
-                        Console.WriteLine("Aktion Ork1 -> Attacke auf Held misslungen" + "\n");
-                    }
-                }
-                //Orc Attacke gelungen - Held Parade fehlgeschlagen
-                else if (AttackrndO <= Orcs[0].Attackvalue && DefendrndH > Heroes[0].Paradevalue)
-                {
-                    Heroes[0].Lifepoints = Heroes[0].Lifepoints - Orcs[0].Damage;
-                    Console.WriteLine($"Aktion Ork -> Attacke gelungen: Hero hat {Heroes[0].Lifepoints} Lebenspunkte " + "\n");
-                }
-                //Orc Attacke parrierd
-                else if (AttackrndO <= Orcs[0].Attackvalue && DefendrndH <= Heroes[0].Paradevalue)
-                {
-                    Console.WriteLine("Aktion Ork -> Angriff vom Ork parriert" + "\n");
-                }
-                // Orc Attacke misslungen
-                else if (AttackrndO > Orcs[0].Attackvalue)
-                {
-                    Console.WriteLine("Aktion Ork -> Attacke auf Held misslungen" + "\n");
-                }
             }
-            Console.WriteLine($"\n\nEndwerte: ");
-            Console.WriteLine($"Daten vom Ork : {Orcs[0].getdateRed()}");
-            Console.WriteLine($"Daten vom Ork 1: {Orcs[1].getdateRed()}");
-            Console.WriteLine($"Daten vom Hero : {Heroes[0].getdateRed()}");
-            Console.WriteLine($"Daten vom Hero 1: {Heroes[1].getdateRed()}");
-
-            Console.ReadKey();
-
 
         }
 
@@ -159,7 +83,7 @@ namespace HumanvsOrc
         static Boolean Action_Done(int SkillValue)
         {
             Random Cube = new Random();
-            int Result = Cube.Next(1, 20); 
+            int Result = Cube.Next(1, 20);
             Console.WriteLine($"Cube: {Result} - SkillValue: {SkillValue}");
 
             if (Result <= SkillValue)
@@ -180,8 +104,8 @@ namespace HumanvsOrc
             return Defender;
 
         }
+
     }
+
 }
-
-
 
--- Underlyings.cs
+++ Underlyings.cs
@@ -6,29 +6,50 @@ using System.Text;
 namespace HumanvsOrc
 {
     class Underlyings
-        //Properties
+    //Properties
     {
         public int Attackvalue { get; set; }
         public int Paradevalue { get; set; }
-        public int Lifepoints { get; set;  }
+        public int Lifepoints { get; set; }
         public int Damage { get; set; }
-        // Konstruktur
-        public Underlyings(int _attackvalue, int _paradevalue, int _lifepoints, int _damage)
+
+        public string Name { get; set; }
+
+        //Konstruktur
+        public Underlyings(int _attackvalue, int _paradevalue, int _lifepoints, int _damage, string _name)
         {
             Attackvalue = _attackvalue;
             Paradevalue = _paradevalue;
             Lifepoints = _lifepoints;
             Damage = _damage;
+            Name = _name;
         }
 
-        //public string getdate()
-        //{
-        //    return "Attacke: " + Attackvalue + ", Parade: " + Paradevalue + ", Lebenspunkte: " + Lifepoints + ", Schaden: " + Damage;
-        //}
+
+        public Boolean Is_Alive()
+        {
+            if (Lifepoints > 0)
+                return true;
+
+            return false;
+        }
+
+
+
+        public string getdate()
+        {
+            return "Attacke: " + Attackvalue + ", Parade: " + Paradevalue + ", Lebenspunkte: " + Lifepoints + ", Schaden: " + Damage + ", Name: " + Name;
+        }
 
         public string getdateRed()
         {
             return "Lebenspunkte: " + Lifepoints + ", Schaden: " + Damage;
         }
+
+
+
+
+
+
     }
 }
